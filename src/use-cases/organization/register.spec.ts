f9b2730f791c9d4f91b18969cc573e9f967d63de
Julio Castro <juliocastro.contato@gmail.com>
2025-10-11 19:46:21 -0300

    feat: add new tests for register use-case

---
+++ src/use-cases/organization/register.spec.ts
@@ -0,0 +1,51 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryOrganizationRepository } from '../repositories/in-memory/in-memory-organization-repository.ts'
+import { CreateOrganizationUseCase } from './register.ts'
+import { randomUUID } from 'node:crypto'
+
+let organizationRepository: InMemoryOrganizationRepository
+let sut: CreateOrganizationUseCase
+
+describe('Unit Test Organization', () => {
+  beforeEach(() => {
+    organizationRepository = new InMemoryOrganizationRepository()
+    sut = new CreateOrganizationUseCase(organizationRepository)
+  })
+
+  it('should create a organization', async () => {
+    const { organization } = await sut.execute({
+      id: randomUUID(),
+      name_responsible: 'Org amigos',
+      address: 'Rua 1',
+      district: 'São Paulo',
+      number: 124,
+      phone: '12345678910',
+      zipcode: 13327464,
+      city: 'SP',
+      cnpj: 52702990000103,
+      created_at: String(new Date()),
+      email: 'org.example@example.com',
+      password: '12345678',
+      role: 'ADMIN',
+    })
+
+    expect(organization).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name_responsible: 'Org amigos',
+        address: 'Rua 1',
+        district: 'São Paulo',
+        number: 124,
+        phone: '12345678910',
+        zipcode: 13327464,
+        city: 'SP',
+        cnpj: 52702990000103,
+        created_at: String(new Date()),
+        email: 'org.example@example.com',
+        password: '12345678',
+        role: 'ADMIN',
+      }),
+    )
+    expect(organization.role).toBe('ADMIN')
+  })
+})
