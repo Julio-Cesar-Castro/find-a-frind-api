eafa2f3a0e7233246959b7946ad7108e3fcf53b0
Julio Castro <juliocastro.contato@gmail.com>
2025-10-11 19:42:38 -0300

    feat: add new fields to interface

--- src/interfaces/organization.ts
+++ src/interfaces/organization.ts
@@ -1,9 +1,15 @@
 export interface Organization {
   id: string
-  name: string
+  name_responsible: string
+  email: string
+  password: string
+  city: string
   address: string
   number: number
   district: string
   zipcode: number
   phone: string
+  created_at: string
+  cnpj: number
+  role: 'ADMIN' | 'USER'
 }
--- src/use-cases/repositories/in-memory/in-memory-organization-repository.ts
+++ src/use-cases/repositories/in-memory/in-memory-organization-repository.ts
@@ -1,20 +1,37 @@
 import type { Organization } from '@/interfaces/organization.ts'
+import type { OrganizationRepository } from '../organization-repository.ts'
 
-export class InMemoryOrganizationRepository {
+export class InMemoryOrganizationRepository implements OrganizationRepository {
   public items: Organization[] = []
 
   async create(data: Organization) {
     const organization = {
       id: data.id,
-      name: data.name,
+      name_responsible: data.name_responsible,
+      email: data.email,
+      password: data.password,
+      city: data.city,
       address: data.address,
       number: data.number,
       district: data.district,
       zipcode: data.zipcode,
       phone: data.phone,
+      created_at: data.created_at,
+      cnpj: data.cnpj,
+      role: data.role,
     }
 
     this.items.push(organization)
+
+    return organization
+  }
+
+  async findByCNPJ(cnpj: number) {
+    const organization = this.items.find((item) => item.cnpj === cnpj)
+
+    if (!organization) {
+      return null
+    }
 
     return organization
   }
