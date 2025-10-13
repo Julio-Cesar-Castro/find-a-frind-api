fa8c5554e6717e8d0b0b4e7f9c576c04c9830e34
Julio Castro <juliocastro.contato@gmail.com>
2025-10-12 22:58:46 -0300

    feat: create findPetById method

--- src/use-cases/repositories/pet-repository.ts
+++ src/use-cases/repositories/pet-repository.ts
@@ -3,4 +3,5 @@ import type { Pet } from '@/interfaces/pet.ts'
 export interface PetRepository {
   create(data: Pet): Promise<Pet>
   findPetByCity(city: string): Promise<Pet[] | null>
+  findPetById(id: string): Promise<Pet | null>
 }
