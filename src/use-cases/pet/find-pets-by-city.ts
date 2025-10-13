464597000031c6bffae586929d7228068f9e12e0
Julio Castro <juliocastro.contato@gmail.com>
2025-10-12 22:56:43 -0300

    fix: modified names

--- src/use-cases/pet/find-pets-by-city.ts
+++ src/use-cases/pet/find-pets-by-city.ts
@@ -1,18 +1,20 @@
 import type { PetRepository } from '../repositories/pet-repository.ts'
 import type { Pet } from '@/interfaces/pet.ts'
 
-interface ListPetRequest {
+interface FindPetByCityRequest {
   city: string
 }
 
-interface ListPetResponse {
+interface FindPetByCityResponse {
   pet: Pet[] | null
 }
 
 export class ListPetUseCase {
   constructor(private petRepository: PetRepository) {}
 
-  async execute({ city }: ListPetRequest): Promise<ListPetResponse> {
+  async execute({
+    city,
+  }: FindPetByCityRequest): Promise<FindPetByCityResponse> {
     const pet = await this.petRepository.findPetByCity(city)
 
     return { pet }
