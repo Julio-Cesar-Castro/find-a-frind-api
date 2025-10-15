565c28ef0c63d48a54796cd397d695f40a1cb666
Julio Castro <juliocastro.contato@gmail.com>
2025-10-14 21:38:06 -0300

    feat: create organization interface organization findbyCNPJ

--- src/use-cases/repositories/pet-repository.ts
+++
@@ -1,7 +0,0 @@
-import type { Pet } from '@/interfaces/pet.ts'
-
-export interface PetRepository {
-  create(data: Pet): Promise<Pet>
-  findPetByCity(city: string): Promise<Pet[] | null>
-  findPetById(id: string): Promise<Pet | null>
-}
--- src/use-cases/repositories/user-repository.ts
+++
@@ -1,6 +0,0 @@
-import type { User } from '@/interfaces/user.ts'
-
-export interface UserRepository {
-  create(data: User): Promise<User>
-  findByEmail(email: string): Promise<User | null>
-}
